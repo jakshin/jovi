48e73c38c904b3ac9eb00bcafca81d0f12bea5a4
Jason Jackson <jasonjackson@pobox.com>
2019-02-26 23:04:16 -0700

    Add tests that use the official text fixtures
    We fail against more of them than we pass against, but we can successfully run against them all now, using both the intepreter and JS transpiler, so that's progress.

---
+++ tests/officialFixtures.test.js
@@ -0,0 +1,102 @@
+const testWithFixture = require("./fixtureUtils/testWithFixture")
+
+describe("comments", () => {
+  test("complex_comments.rock", () => testWithFixture("official/comments/complex_comments.rock"))
+  test("simpleComments.rock", () => testWithFixture("official/comments/simpleComments.rock"))
+  test("simple_comment.rock", () => testWithFixture("official/comments/simple_comment.rock"))
+})
+
+describe("conditionals", () => {
+  test("empty_if.rock", () => testWithFixture("official/conditionals/empty_if.rock"))
+  test("simpleConditionals.rock", () => testWithFixture("official/conditionals/simpleConditionals.rock"))
+  test("truthinessTest.rock", () => testWithFixture("official/conditionals/truthinessTest.rock"))
+})
+
+describe("constants", () => {
+  test("constants.rock", () => testWithFixture("official/constants/constants.rock"))
+})
+
+describe("control-flow", () => {
+  test("nested_loops.rock", () => testWithFixture("official/control-flow/nested_loops.rock"))
+  test("simpleLoops.rock", () => testWithFixture("official/control-flow/simpleLoops.rock"))
+})
+
+describe("equality", () => {
+  test("booleans.rock", () => testWithFixture("official/equality/booleans.rock"))
+  test("equalityComparison.rock", () => testWithFixture("official/equality/equalityComparison.rock"))
+  test("mysterious.rock", () => testWithFixture("official/equality/mysterious.rock"))
+  test("negation.rock", () => testWithFixture("official/equality/negation.rock"))
+  test("nothing.rock", () => testWithFixture("official/equality/nothing.rock"))
+  test("null.rock", () => testWithFixture("official/equality/null.rock"))
+  test("numbers.rock", () => testWithFixture("official/equality/numbers.rock"))
+  test("strings.rock", () => testWithFixture("official/equality/strings.rock"))
+})
+
+describe("examples", () => {
+  test("99_beers.rock", () => testWithFixture("official/examples/99_beers.rock"))
+  test("factorial.rock", () => testWithFixture("official/examples/factorial.rock"))
+  test("fibonacci.rock", () => testWithFixture("official/examples/fibonacci.rock"))
+  test("fizzbuzz-idiomatic.rock", () => testWithFixture("official/examples/fizzbuzz-idiomatic.rock"))
+  test("fizzbuzz-minimalist.rock", () => testWithFixture("official/examples/fizzbuzz-minimalist.rock"))
+})
+
+describe("functions", () => {
+  test("functionCalls.rock", () => testWithFixture("official/functions/functionCalls.rock"))
+  test("nested_function_scopes.rock", () => testWithFixture("official/functions/nested_function_scopes.rock"))
+  test("nested_functions.rock", () => testWithFixture("official/functions/nested_functions.rock"))
+  test("recursion.rock", () => testWithFixture("official/functions/recursion.rock"))
+  test("simpleFunctions.rock", () => testWithFixture("official/functions/simpleFunctions.rock"))
+})
+
+describe("io", () => {
+  test("hello_number.rock", () => testWithFixture("official/io/hello_number.rock"))
+  test("hello_world.rock", () => testWithFixture("official/io/hello_world.rock"))
+  test("inputTest.rock", () => testWithFixture("official/io/inputTest.rock"))
+  test("inputTest2.rock", () => testWithFixture("official/io/inputTest2.rock"))
+})
+
+describe("literals", () => {
+  test("literalAliases.rock", () => testWithFixture("official/literals/literalAliases.rock"))
+  test("literalstrings.rock", () => testWithFixture("official/literals/literalstrings.rock"))
+  test("poeticLiterals.rock", () => testWithFixture("official/literals/poeticLiterals.rock"))
+  test("poeticNumbers.rock", () => testWithFixture("official/literals/poeticNumbers.rock"))
+})
+
+describe("math", () => {
+  test("operator_aliases.rock", () => testWithFixture("official/math/operator_aliases.rock"))
+  test("operator_precedence.rock", () => testWithFixture("official/math/operator_precedence.rock"))
+  test("operators.rock", () => testWithFixture("official/math/operators.rock"))
+})
+
+describe("operators", () => {
+  test("addOperator.rock", () => testWithFixture("official/operators/addOperator.rock"))
+  test("andTest.rock", () => testWithFixture("official/operators/andTest.rock"))
+  test("booleans.rock", () => testWithFixture("official/operators/booleans.rock"))
+  test("divisionOperator.rock", () => testWithFixture("official/operators/divisionOperator.rock"))
+  test("incrementAndDecrement.rock", () => testWithFixture("official/operators/incrementAndDecrement.rock"))
+  test("multiplicationOperator.rock", () => testWithFixture("official/operators/multiplicationOperator.rock"))
+  test("notTest.rock", () => testWithFixture("official/operators/notTest.rock"))
+  test("orNorTest.rock", () => testWithFixture("official/operators/orNorTest.rock"))
+  test("orderingComparison.rock", () => testWithFixture("official/operators/orderingComparison.rock"))
+  test("subtractOperator.rock", () => testWithFixture("official/operators/subtractOperator.rock"))
+})
+
+describe("variables", () => {
+  test("common_variables.rock", () => testWithFixture("official/variables/common_variables.rock"))
+  test("globalVariables.rock", () => testWithFixture("official/variables/globalVariables.rock"))
+  test("poeticStrings.rock", () => testWithFixture("official/variables/poeticStrings.rock"))
+  test("pronouns.rock", () => testWithFixture("official/variables/pronouns.rock"))
+  test("proper_variables.rock", () => testWithFixture("official/variables/proper_variables.rock"))
+  test("simple_pronouns.rock", () => testWithFixture("official/variables/simple_pronouns.rock"))
+  test("umlauts.rock", () => testWithFixture("official/variables/umlauts.rock"))
+})
+
+describe("whitespace", () => {
+  test("apostrophesIgnored.rock", () => testWithFixture("official/whitespace/apostrophesIgnored.rock"))
+  test("leading_blank_lines.rock", () => testWithFixture("official/whitespace/leading_blank_lines.rock"))
+  test("leading_empty_lines.rock", () => testWithFixture("official/whitespace/leading_empty_lines.rock"))
+  test("leading_whitespace.rock", () => testWithFixture("official/whitespace/leading_whitespace.rock"))
+  test("no_newline_at_eof.rock", () => testWithFixture("official/whitespace/no_newline_at_eof.rock"))
+  test("trailing_blank_lines.rock", () => testWithFixture("official/whitespace/trailing_blank_lines.rock"))
+  test("trailing_empty_lines.rock", () => testWithFixture("official/whitespace/trailing_empty_lines.rock"))
+})
---
+++ tests/officialFixturesInJS.test.js
@@ -0,0 +1,102 @@
+const testWithFixture = require("./fixtureUtils/testWithFixture")
+
+describe("comments", () => {
+  test("complex_comments.rock converted to JS", () => testWithFixture("official/comments/complex_comments.rock", "JS"))
+  test("simpleComments.rock converted to JS", () => testWithFixture("official/comments/simpleComments.rock", "JS"))
+  test("simple_comment.rock converted to JS", () => testWithFixture("official/comments/simple_comment.rock", "JS"))
+})
+
+describe("conditionals", () => {
+  test("empty_if.rock converted to JS", () => testWithFixture("official/conditionals/empty_if.rock", "JS"))
+  test("simpleConditionals.rock converted to JS", () => testWithFixture("official/conditionals/simpleConditionals.rock", "JS"))
+  test("truthinessTest.rock converted to JS", () => testWithFixture("official/conditionals/truthinessTest.rock", "JS"))
+})
+
+describe("constants", () => {
+  test("constants.rock converted to JS", () => testWithFixture("official/constants/constants.rock", "JS"))
+})
+
+describe("control-flow", () => {
+  test("nested_loops.rock converted to JS", () => testWithFixture("official/control-flow/nested_loops.rock", "JS"))
+  test("simpleLoops.rock converted to JS", () => testWithFixture("official/control-flow/simpleLoops.rock", "JS"))
+})
+
+describe("equality", () => {
+  test("booleans.rock converted to JS", () => testWithFixture("official/equality/booleans.rock", "JS"))
+  test("equalityComparison.rock converted to JS", () => testWithFixture("official/equality/equalityComparison.rock", "JS"))
+  test("mysterious.rock converted to JS", () => testWithFixture("official/equality/mysterious.rock", "JS"))
+  test("negation.rock converted to JS", () => testWithFixture("official/equality/negation.rock", "JS"))
+  test("nothing.rock converted to JS", () => testWithFixture("official/equality/nothing.rock", "JS"))
+  test("null.rock converted to JS", () => testWithFixture("official/equality/null.rock", "JS"))
+  test("numbers.rock converted to JS", () => testWithFixture("official/equality/numbers.rock", "JS"))
+  test("strings.rock converted to JS", () => testWithFixture("official/equality/strings.rock", "JS"))
+})
+
+describe("examples", () => {
+  test("99_beers.rock converted to JS", () => testWithFixture("official/examples/99_beers.rock", "JS"))
+  test("factorial.rock converted to JS", () => testWithFixture("official/examples/factorial.rock", "JS"))
+  test("fibonacci.rock converted to JS", () => testWithFixture("official/examples/fibonacci.rock", "JS"))
+  test("fizzbuzz-idiomatic.rock converted to JS", () => testWithFixture("official/examples/fizzbuzz-idiomatic.rock", "JS"))
+  test("fizzbuzz-minimalist.rock converted to JS", () => testWithFixture("official/examples/fizzbuzz-minimalist.rock", "JS"))
+})
+
+describe("functions", () => {
+  test("functionCalls.rock converted to JS", () => testWithFixture("official/functions/functionCalls.rock", "JS"))
+  test("nested_function_scopes.rock converted to JS", () => testWithFixture("official/functions/nested_function_scopes.rock", "JS"))
+  test("nested_functions.rock converted to JS", () => testWithFixture("official/functions/nested_functions.rock", "JS"))
+  test("recursion.rock converted to JS", () => testWithFixture("official/functions/recursion.rock", "JS"))
+  test("simpleFunctions.rock converted to JS", () => testWithFixture("official/functions/simpleFunctions.rock", "JS"))
+})
+
+describe("io", () => {
+  test("hello_number.rock converted to JS", () => testWithFixture("official/io/hello_number.rock", "JS"))
+  test("hello_world.rock converted to JS", () => testWithFixture("official/io/hello_world.rock", "JS"))
+  test("inputTest.rock converted to JS", () => testWithFixture("official/io/inputTest.rock", "JS"))
+  test("inputTest2.rock converted to JS", () => testWithFixture("official/io/inputTest2.rock", "JS"))
+})
+
+describe("literals", () => {
+  test("literalAliases.rock converted to JS", () => testWithFixture("official/literals/literalAliases.rock", "JS"))
+  test("literalstrings.rock converted to JS", () => testWithFixture("official/literals/literalstrings.rock", "JS"))
+  test("poeticLiterals.rock converted to JS", () => testWithFixture("official/literals/poeticLiterals.rock", "JS"))
+  test("poeticNumbers.rock converted to JS", () => testWithFixture("official/literals/poeticNumbers.rock", "JS"))
+})
+
+describe("math", () => {
+  test("operator_aliases.rock converted to JS", () => testWithFixture("official/math/operator_aliases.rock", "JS"))
+  test("operator_precedence.rock converted to JS", () => testWithFixture("official/math/operator_precedence.rock", "JS"))
+  test("operators.rock converted to JS", () => testWithFixture("official/math/operators.rock", "JS"))
+})
+
+describe("operators", () => {
+  test("addOperator.rock converted to JS", () => testWithFixture("official/operators/addOperator.rock", "JS"))
+  test("andTest.rock converted to JS", () => testWithFixture("official/operators/andTest.rock", "JS"))
+  test("booleans.rock converted to JS", () => testWithFixture("official/operators/booleans.rock", "JS"))
+  test("divisionOperator.rock converted to JS", () => testWithFixture("official/operators/divisionOperator.rock", "JS"))
+  test("incrementAndDecrement.rock converted to JS", () => testWithFixture("official/operators/incrementAndDecrement.rock", "JS"))
+  test("multiplicationOperator.rock converted to JS", () => testWithFixture("official/operators/multiplicationOperator.rock", "JS"))
+  test("notTest.rock converted to JS", () => testWithFixture("official/operators/notTest.rock", "JS"))
+  test("orNorTest.rock converted to JS", () => testWithFixture("official/operators/orNorTest.rock", "JS"))
+  test("orderingComparison.rock converted to JS", () => testWithFixture("official/operators/orderingComparison.rock", "JS"))
+  test("subtractOperator.rock converted to JS", () => testWithFixture("official/operators/subtractOperator.rock", "JS"))
+})
+
+describe("variables", () => {
+  test("common_variables.rock converted to JS", () => testWithFixture("official/variables/common_variables.rock", "JS"))
+  test("globalVariables.rock converted to JS", () => testWithFixture("official/variables/globalVariables.rock", "JS"))
+  test("poeticStrings.rock converted to JS", () => testWithFixture("official/variables/poeticStrings.rock", "JS"))
+  test("pronouns.rock converted to JS", () => testWithFixture("official/variables/pronouns.rock", "JS"))
+  test("proper_variables.rock converted to JS", () => testWithFixture("official/variables/proper_variables.rock", "JS"))
+  test("simple_pronouns.rock converted to JS", () => testWithFixture("official/variables/simple_pronouns.rock", "JS"))
+  test("umlauts.rock converted to JS", () => testWithFixture("official/variables/umlauts.rock", "JS"))
+})
+
+describe("whitespace", () => {
+  test("apostrophesIgnored.rock converted to JS", () => testWithFixture("official/whitespace/apostrophesIgnored.rock", "JS"))
+  test("leading_blank_lines.rock converted to JS", () => testWithFixture("official/whitespace/leading_blank_lines.rock", "JS"))
+  test("leading_empty_lines.rock converted to JS", () => testWithFixture("official/whitespace/leading_empty_lines.rock", "JS"))
+  test("leading_whitespace.rock converted to JS", () => testWithFixture("official/whitespace/leading_whitespace.rock", "JS"))
+  test("no_newline_at_eof.rock converted to JS", () => testWithFixture("official/whitespace/no_newline_at_eof.rock", "JS"))
+  test("trailing_blank_lines.rock converted to JS", () => testWithFixture("official/whitespace/trailing_blank_lines.rock", "JS"))
+  test("trailing_empty_lines.rock converted to JS", () => testWithFixture("official/whitespace/trailing_empty_lines.rock", "JS"))
+})
