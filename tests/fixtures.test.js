057e0c1415ddcc02ac2724ba78c7146717684d64
Jason Jackson <jasonjackson@pobox.com>
2019-02-28 22:53:24 -0700

    Implement proper semantics for decrementing

--- tests/fixtures.test.js
+++ tests/fixtures.test.js
@@ -1,5 +1,17 @@
 const testWithFixture = require("./fixtureUtils/testWithFixture")
 
+describe("decrement", () => {
+  test("decrement.rock", () => testWithFixture("decrement/decrement.rock"))
+  test("decrementBooleanLiteral.rock", () => testWithFixture("decrement/decrementBooleanLiteral.rock"))
+  test("decrementFloatLiteral.rock", () => testWithFixture("decrement/decrementFloatLiteral.rock"))
+  test("decrementIntegerLiteral.rock", () => testWithFixture("decrement/decrementIntegerLiteral.rock"))
+  test("decrementMysteriousLiteral.rock", () => testWithFixture("decrement/decrementMysteriousLiteral.rock"))
+  test("decrementMysteriousVariable.rock", () => testWithFixture("decrement/decrementMysteriousVariable.rock"))
+  test("decrementNullLiteral.rock", () => testWithFixture("decrement/decrementNullLiteral.rock"))
+  test("decrementStringLiteral.rock", () => testWithFixture("decrement/decrementStringLiteral.rock"))
+  test("decrementStringVariable.rock", () => testWithFixture("decrement/decrementStringVariable.rock"))
+})
+
 describe("increment", () => {
   test("increment.rock", () => testWithFixture("increment/increment.rock"))
   test("incrementBooleanLiteral.rock", () => testWithFixture("increment/incrementBooleanLiteral.rock"))
--- tests/fixturesInJS.test.js
+++ tests/fixturesInJS.test.js
@@ -1,5 +1,17 @@
 const testWithFixture = require("./fixtureUtils/testWithFixture")
 
+describe("decrement", () => {
+  test("decrement.rock converted to JS", () => testWithFixture("decrement/decrement.rock", "JS"))
+  test("decrementBooleanLiteral.rock converted to JS", () => testWithFixture("decrement/decrementBooleanLiteral.rock", "JS"))
+  test("decrementFloatLiteral.rock converted to JS", () => testWithFixture("decrement/decrementFloatLiteral.rock", "JS"))
+  test("decrementIntegerLiteral.rock converted to JS", () => testWithFixture("decrement/decrementIntegerLiteral.rock", "JS"))
+  test("decrementMysteriousLiteral.rock converted to JS", () => testWithFixture("decrement/decrementMysteriousLiteral.rock", "JS"))
+  test("decrementMysteriousVariable.rock converted to JS", () => testWithFixture("decrement/decrementMysteriousVariable.rock", "JS"))
+  test("decrementNullLiteral.rock converted to JS", () => testWithFixture("decrement/decrementNullLiteral.rock", "JS"))
+  test("decrementStringLiteral.rock converted to JS", () => testWithFixture("decrement/decrementStringLiteral.rock", "JS"))
+  test("decrementStringVariable.rock converted to JS", () => testWithFixture("decrement/decrementStringVariable.rock", "JS"))
+})
+
 describe("increment", () => {
   test("increment.rock converted to JS", () => testWithFixture("increment/increment.rock", "JS"))
   test("incrementBooleanLiteral.rock converted to JS", () => testWithFixture("increment/incrementBooleanLiteral.rock", "JS"))
